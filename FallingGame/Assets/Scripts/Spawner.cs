662425188ed73b2cb0eb760480f0d75b0e98b66d
Diya Yang <yangdiya.cs@gmail.com>
2022-11-27 20:26:23 -0500

    third version

--- FallingGame/Assets/Scripts/Spawner.cs
+++ FallingGame/Assets/Scripts/Spawner.cs
@@ -16,37 +16,67 @@ public class Spawner : MonoBehaviour
         SpawnPlatform();
     }
 
+    // public void SpawnPlatform(){
+    //     countTime += Time.deltaTime;
+    //     spawnPosition = transform.position;
+    //     spawnPosition.x = Random.Range(-3.5f, 3.5f);
+
+    //     if(countTime >= spawnTime)
+    //     {
+    //         Create();
+    //         countTime = 0;
+
+    //     }
     public void SpawnPlatform(){
-        countTime += Time.deltaTime;
+        countTime +=  Time.deltaTime;
         spawnPosition = transform.position;
-        spawnPosition.x = Random.Range(-3.5f, 3.5f);
+        spawnPosition.x = Random.Range(-4f, 4f);
 
-        if(countTime >= spawnTime)
-        {
-            Create();
+        if(countTime >= spawnTime){
+            CreatePlatform();
             countTime = 0;
-
         }
-
-
     }
+    // }
 
-    public void Create()
-    {
+    // public void Create()
+    // {
+    //     int index = Random.Range(0, platforms.Count);
+    //     // int spikeNum = 0;
+
+    //     // if (index == 4){
+    //     //     spikeNum ++;
+    //     // }
+
+    //     // if (spikeNum > 1){
+    //     //     spikeNum = 0;
+    //     //     countTime = spawnTime;
+    //     //     return;
+    //     // }
+
+    //     GameObject newPlatform = Instantiate(platforms[index], spawnPosition, Quaternion.identity);
+    //     newPlatform.transform.SetParent(this.gameObject.transform);
+    // }
+    public void CreatePlatform(){
         int index = Random.Range(0, platforms.Count);
-        // int spikeNum = 0;
-
-        // if (index == 4){
-        //     spikeNum ++;
-        // }
-
-        // if (spikeNum > 1){
-        //     spikeNum = 0;
-        //     countTime = spawnTime;
-        //     return;
-        // }
-
-        GameObject newPlatform = Instantiate(platforms[index], spawnPosition, Quaternion.identity);
+        int spikeNum = 0;
+        if(index == 4){
+            spikeNum++;
+        }
+        if(spikeNum > 1){
+            spikeNum = 0;
+            countTime = spawnTime;
+            return;
+        }
+        
+        GameObject newPlatform;
+        if(index == 4){
+            spawnPosition.x = Random.Range(-1.8f, 5.5f);
+            newPlatform = Instantiate(platforms[4], spawnPosition, Quaternion.identity);
+        }
+        else{
+            newPlatform = Instantiate(platforms[index], spawnPosition, Quaternion.identity);
+        }
         newPlatform.transform.SetParent(this.gameObject.transform);
     }
 }
